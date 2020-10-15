81ae78955f094ea6b660013e4b57b0280c376d56
DavidCorreas <davidestu77@gmail.com>
2020-10-16 01:19:58 +0200

    Added: set up emulator

--- setup-emulator.sh
+++ setup-emulator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t setup-emu_docker -f ./tfg-setup/emu-setup-dockerfile .
-docker run -it --rm -v $(pwd)/tfg-elastest-emulator:/tmp/emu-docker -v /var/run/docker.sock:/var/run/docker.sock setup-emu_docker emu-docker interactive
+docker run --rm -it -v $(pwd)/tfg-elastest-emulator:/tmp/emu-docker -v /var/run/docker.sock:/var/run/docker.sock setup-emu_docker
 docker rmi setup-emu_docker
