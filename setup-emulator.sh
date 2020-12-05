114f98f42de47227cc0991e8dda33328c47e8cbb
DavidCorreas <davidestu77@gmail.com>
2020-12-05 17:35:46 +0100

    Added: Permissions to setup

--- setup-emulator.sh
+++ setup-emulator.sh
@@ -7,10 +7,11 @@ if [ "$(find ~/.android -mindepth 1 2>/dev/null | wc -w)" -lt 3 ]
 then
     docker run --rm -v ~/.android:/root/.android tfgelastest/appium adb devices
     [ "$UID" -eq 0 ] || echo "Running root permissions to assign permissions to ~/.android"
-    [ "$UID" -eq 0 ] || exec sudo chown -R $ADB_USER $ADB_HOME/.android 
+    [ "$UID" -eq 0 ] || sudo chown -R $ADB_USER $ADB_HOME/.android && ./setup-emulator.sh
 fi
 
 # Setup emulator
-docker build -t setup-emu_docker -f ./tfg-setup/emu-setup-dockerfile .
-docker run --rm -it -v $(pwd)/tfg-elastest-emulator:/tmp/emu-docker -v $ADB_HOME/.android:/root/.android -v /var/run/docker.sock:/var/run/docker.sock setup-emu_docker
+[ "$UID" -eq 0 ] || echo "Running root permissions to build the container"
+[ "$UID" -eq 0 ] || sudo docker build -t setup-emu_docker -f ./tfg-setup/emu-setup-dockerfile .
+docker run --rm -it -v $(pwd)/tfg-elastest-emulator:/tmp/emu-docker -v $ADB_HOME/.android:/root/.android -v /var/run/docker.sock:/var/run/docker.sock setup-emu_docker && \
 docker rmi setup-emu_docker
