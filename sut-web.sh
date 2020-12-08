b5aff89962fd4e954e5805a089190b88a24b99ee
DavidCorreas <davidestu77@gmail.com>
2020-12-08 16:56:17 +0100

    Fixed: network envoymesh

--- sut-web.sh
+++ sut-web.sh
@@ -19,6 +19,15 @@ function printAndSleep {
 
 # Levanta el sut, tanto back como front web
 printAndSleep "SUT"
+
+if [ "$mode" != "down" ]; then
+    docker network create tfg-elastest_envoymesh
+fi
+
 docker-compose -f $current_dir/tfg-elastest-sut/docker-compose.yml -f $current_dir/tfg-elastest-sut/development-compose.yml --env-file=$current_dir/tfg-elastest-sut/.env $mode
+
+if [ "$mode" == "down" ]; then
+    docker network rm tfg-elastest_envoymesh
+fi
 
 exit 0
