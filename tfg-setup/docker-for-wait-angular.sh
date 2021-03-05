7de40d07352949a71d3bb5667f5220ab96a85a5c
DavidCorreas <davidestu77@gmail.com>
2021-03-05 11:37:53 +0100

    dont remove image

--- tfg-setup/docker-for-wait-angular.sh
+++ tfg-setup/docker-for-wait-angular.sh
@@ -4,4 +4,4 @@ current_dir="$(dirname $(realpath "$0"))"
 # Setup emulator
 docker build -t wait-for-angular -f $current_dir/wget-dockerfile $current_dir
 docker run --rm -it --network tfg-elastest_sut wait-for-angular
-docker rmi wait-for-angular
+# docker rmi wait-for-angular
