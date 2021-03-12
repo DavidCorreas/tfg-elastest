a01d71faab8fe5ac952f2b33f4463a38894b348d
DavidCorreas <davidestu77@gmail.com>
2021-03-12 13:57:15 +0100

    Added: new apk 10.0.0.2

--- sut-web.sh
+++ sut-web.sh
@@ -26,8 +26,8 @@ fi
 
 docker-compose -f $current_dir/tfg-elastest-sut/docker-compose.yml -f $current_dir/tfg-elastest-sut/development-compose.yml --env-file=$current_dir/tfg-elastest-sut/.env $mode
 
-if [ "$mode" == "down" ]; then
-    # docker network rm tfg-elastest_envoymesh
-fi
+# if [ "$mode" == "down" ]; then
+#     docker network rm tfg-elastest_envoymesh
+# fi
 
 exit 0
